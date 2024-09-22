76e27a718dda9cacbfc9c455aca787898a76d7c1
VKTRenokh <viktorenokh@gmail.com>
2024-09-22 21:23:41 +0300

    feat: implement lazy playlist struct

--- src/playlists.zig
+++ src/playlists.zig
@@ -3,16 +3,33 @@ const fs = std.fs;
 
 pub const Playlist = struct {
     path: []const u8,
-    content: ?[][]const u8,
+    content: ?[][]const u8 = null,
 
-    pub fn load(self: Playlist, allocator: std.mem.Allocator) ![][]const u8 {
+    pub fn load(self: *Playlist, allocator: std.mem.Allocator) ![][]const u8 {
         if (self.content) |content| {
             return content;
         }
 
+        var content = std.ArrayList([]const u8).init(allocator);
+
         const file = try std.fs.openFileAbsolute(self.path, .{});
 
-        const content = try file.readToEndAlloc(allocator, 2 ** 64);
+        var iterator = std.mem.splitAny(
+            u8,
+            try file.readToEndAlloc(
+                allocator,
+                try file.getEndPos(),
+            ),
+            "\n",
+        );
+
+        while (iterator.next()) |item| {
+            try content.append(item);
+        }
+
+        self.content = content.items;
+
+        return content.items;
     }
 };
 
@@ -24,15 +41,19 @@ pub fn isDir(cwd: std.fs.Dir, path: []const u8) bool {
     return stat.kind == .directory;
 }
 
-pub fn getPlaylists(allocator: std.mem.Allocator, paths: [][]const u8) ![]bool {
-    var list = std.ArrayList(bool).init(allocator);
+pub fn getPlaylists(allocator: std.mem.Allocator, paths: [][]const u8) ![]Playlist {
+    var list = std.ArrayList(Playlist).init(allocator);
 
     const cwd = fs.cwd();
 
     for (paths) |path| {
         const dir = isDir(cwd, path);
 
-        try list.append(dir);
+        if (!dir) {
+            const playlist = Playlist{ .path = path };
+            _ = try playlist.load(allocator);
+            try list.append(playlist);
+        }
     }
 
     return list.items;
