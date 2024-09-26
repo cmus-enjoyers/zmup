c71be8c63f2dcbdad9ef5cd44a75ceae64bbf472
VKTRenokh <viktorenokh@gmail.com>
2024-09-26 14:47:47 +0300

    refactor: reduce nesting

--- src/track.zig
+++ src/track.zig
@@ -1,43 +1,47 @@
 const c = @import("root.zig").c;
 const std = @import("std");
 
+pub fn getMetadata(metadata: *c.AVDictionary) void {
+    var tag: ?*c.AVDictionaryEntry = null;
+    while (true) {
+        tag = c.av_dict_get(metadata, "", tag, c.AV_DICT_IGNORE_SUFFIX);
+        if (tag == null) break;
+
+        const key = tag.?.key;
+        const value = tag.?.value;
+        if (key != null and value != null) {
+            const keyStr = std.mem.span(key);
+            const valueStr = std.mem.span(value);
+            std.debug.print("{s}: {s}\n", .{ keyStr, valueStr });
+        }
+    }
+}
+
 pub fn testFf() !void {
-    const file = "/home/vktrenokh/Music/vk_____________treenokh/Isolation [O07SX0BliAQ].mp3";
+    const file = "/home/vktrenokh/Music/jump/ridge-racer-type-4/01 Urban Fragments.flac";
 
     _ = c.avformat_network_init();
 
-    var formatCtx: ?*c.AVFormatContext = null;
+    var format_ctx: ?*c.AVFormatContext = null;
 
-    if (c.avformat_open_input(&formatCtx, file.ptr, null, null) != 0) {
+    if (c.avformat_open_input(&format_ctx, file.ptr, null, null) != 0) {
+        std.debug.print("no open input", .{});
         return;
     }
-    defer c.avformat_close_input(&formatCtx);
+    defer c.avformat_close_input(&format_ctx);
 
-    if (formatCtx == null) {
-        return;
-    }
-
-    // Retrieve stream information
-    if (c.avformat_find_stream_info(formatCtx, null) < 0) {
+    if (format_ctx == null) {
+        std.debug.print("no format ctx", .{});
         return;
     }
 
-    // Extract metadata from the format context
-    const metadata = formatCtx.?.metadata;
-    if (metadata != null) {
-        var tag: ?*c.AVDictionaryEntry = null;
-        while (true) {
-            tag = c.av_dict_get(metadata, "", tag, c.AV_DICT_IGNORE_SUFFIX);
-            if (tag == null) break;
+    if (c.avformat_find_stream_info(format_ctx, null) < 0) {
+        std.debug.print("no stream info", .{});
+        return;
+    }
 
-            const key = tag.?.key;
-            const value = tag.?.value;
-            if (key != null and value != null) {
-                const keyStr = std.mem.span(key);
-                const valueStr = std.mem.span(value);
-                std.debug.print("{s}: {s}\n", .{ keyStr, valueStr });
-            }
-        }
+    if (format_ctx.?.metadata) |metadata| {
+        getMetadata(metadata);
     } else {
         std.debug.print("No metadata found.\n", .{});
     }
