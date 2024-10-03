df6e7ed3804de4714c72294b88cc8c0445b685b3
VKTRenokh <viktorenokh@gmail.com>
2024-10-03 15:46:08 +0300

    feat: add playlist window

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const playlists = @import("playlists/playlists.zig");
 const sorting = @import("playlists/sorting.zig");
 const c = @import("root.zig").c;
 const time = @import("misc/time.zig");
+const colors = @import("misc/colors.zig");
 
 const Cell = vaxis.Cell;
 const TextInput = vaxis.widgets.TextInput;
@@ -22,29 +23,26 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
 
-    // var tty = try vaxis.Tty.init();
-    // defer tty.deinit();
-    //
-    // var vx = try vaxis.init(allocator, .{});
-    // defer vx.deinit(allocator, tty.anyWriter());
-    //
-    // var loop: vaxis.Loop(Event) = .{
-    //     .tty = &tty,
-    //     .vaxis = &vx,
-    // };
-    // try loop.init();
-    //
-    // try loop.start();
-    // defer loop.stop();
-    //
-    // const any_writer = tty.anyWriter();
-    //
-    // try vx.enterAltScreen(any_writer);
-    //
-    // var text_input = TextInput.init(allocator, &vx.unicode);
-    // defer text_input.deinit();
-    //
-    // try vx.queryTerminal(any_writer, 1 * std.time.ns_per_s);
+    var tty = try vaxis.Tty.init();
+    defer tty.deinit();
+
+    var vx = try vaxis.init(allocator, .{});
+    defer vx.deinit(allocator, tty.anyWriter());
+
+    var loop: vaxis.Loop(Event) = .{
+        .tty = &tty,
+        .vaxis = &vx,
+    };
+    try loop.init();
+
+    try loop.start();
+    defer loop.stop();
+
+    const any_writer = tty.anyWriter();
+
+    try vx.enterAltScreen(any_writer);
+
+    try vx.queryTerminal(any_writer, 1 * std.time.ns_per_s);
 
     c.av_log_set_level(c.AV_LOG_QUIET);
     const home = std.posix.getenv("HOME");
@@ -61,49 +59,29 @@ pub fn main() !void {
         music.deinit();
     }
 
-    // while (true) {
-    //     const event = loop.nextEvent();
-    //
-    //     switch (event) {
-    //         .key_press => |key| {
-    //             if (key.matches('q', .{})) {
-    //                 break;
-    //             } else {
-    //                 try text_input.update(.{ .key_press = key });
-    //             }
-    //         },
-    //         .winsize => |ws| try vx.resize(allocator, any_writer, ws),
-    //         else => {},
-    //     }
-    //
-    //     const win = vx.window();
-    //
-    //     // win.clear();
-    //
-    //     const style: vaxis.Style = .{
-    //         .fg = .{ .index = 100 },
-    //     };
-    //
-    //     const child = win.child(.{
-    //         .x_off = win.width / 2 - 20,
-    //         .y_off = win.height / 2 - 3,
-    //         .width = .{ .limit = 40 },
-    //         .height = .{ .limit = 3 },
-    //         .border = .{
-    //             .where = .all,
-    //             .style = style,
-    //         },
-    //     });
-    //
-    //     text_input.draw(child);
-    //
-    //     try ui.drawSimpleTable(allocator, win);
-    //
-    //     for (x.items, 0..) |item, i| {
-    //         _ = item;
-    //         try ui.drawText(win, "hello", 0, i);
-    //     }
-    //
-    //     // try vx.render(any_writer);
-    // }
+    while (true) {
+        const event = loop.nextEvent();
+
+        switch (event) {
+            .key_press => |key| {
+                if (key.matches('q', .{})) {
+                    break;
+                }
+            },
+            .winsize => |ws| try vx.resize(allocator, any_writer, ws),
+            else => {},
+        }
+
+        const win = vx.window();
+        _ = win.child(.{
+            .border = .{
+                .style = .{ .fg = .{ .rgb = colors.white_rgb } },
+                .where = .all,
+            },
+            .width = .{ .limit = 30 },
+            .height = .{ .limit = win.height },
+        });
+
+        try vx.render(any_writer);
+    }
 }
---
+++ src/misc/colors.zig
@@ -0,0 +1,1 @@
+pub const white_rgb = .{ 255, 255, 255 };
